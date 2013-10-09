591115a25165f31df33b3c5b45fa77d7edf9edf6
David Shariff <shariff.david@gmail.com>
2013-10-09 21:04:31 +0800

    Add delete function to heap

--- Data Structures/MaxHeap.js
+++ Data Structures/MaxHeap.js
@@ -34,6 +34,48 @@
         
     };
     
+    MaxHeap.prototype.deleteMax = function() {
+        
+        // remove current root and replace with last node
+        this.items[0] = this.items[this.size];
+        this.items.pop();
+        this.size--;
+        
+        var current = this.items[0],
+            currPos = 0,
+            indexLeft, indexRight, indexReplace;
+        
+        while (current) {
+            
+            indexLeft  = (2 * currPos) + 1;
+            indexRight = (2 * currPos) + 2;
+            
+            if (this.items[indexLeft] > this.items[indexRight] && 
+                this.items[indexLeft] > current) {
+                
+                indexReplace = indexLeft;
+                
+            }
+            else if (this.items[indexRight] > this.items[indexLeft] && 
+                     this.items[indexRight] > current) {
+                
+                indexReplace = indexRight;
+                
+            }
+            else {
+                break;
+            }
+            
+            // swap the current node with the greater child node
+            this.items[currPos]      = this.items[indexReplace];
+            this.items[indexReplace] = current;
+            currPos = indexReplace;
+            current = this.items[currPos];
+            
+        }
+        
+    };
+    
     MaxHeap.prototype.findParentIndex = function(index) {
         return Math.floor((index - 1) / 2);
     };
@@ -57,5 +99,11 @@
     
     // [25, 19, 20, 8, 13, 9, 7, 6, 2, 1, 5, 3]
     heap.insert(25);
+    
+    // [20, 19, 9, 8, 13, 3, 7, 6, 2, 1, 5]
+    heap.deleteMax();
+    
+    // [19, 13, 9, 8, 5, 3, 7, 6, 2, 1]
+    heap.deleteMax();
     
 }());
